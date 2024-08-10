a112ac0eabfd269a85701d00dcd7d75e96cefda0
Ian <ichan@moneylion.com>
2024-08-10 13:34:09 +0800

    feat: setup new routes for os stats

--- app/backend/main.go
+++ app/backend/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-}
+// OSMetrics struct implements the MetricsHandler interface.
+type OSMetrics struct{}
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+func main() {
+	osstats := &OSMetrics{}
+
+	// Helloworld endpoint
+	http.HandleFunc("/", getHelloString)
+
+	// OS Stats endpoints
+	http.HandleFunc("/memory", osstats.MemoryMetrics)
+	http.HandleFunc("/cpu", osstats.CPUMetrics)
+	http.HandleFunc("/network", osstats.NetworkMetrics)
+	http.HandleFunc("/uptime", osstats.UptimeMetrics)
+
+	// Prometheus endpoint
+	http.Handle("/metrics", promhttp.Handler())
+
+	fmt.Println("Serving requests on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
 }
