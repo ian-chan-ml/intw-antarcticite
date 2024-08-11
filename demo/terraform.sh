3e959b76be61bfdbb71478e4ed8c87e4e28d3140
Ian <ichan@moneylion.com>
2024-08-11 21:49:48 +0800

    chore: prepare demo for part 4 terraform

---
+++ demo/terraform.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+aws configure sso-session
+
+aws sso login --sso-session quanianitis
+
+# assignment/technology/account/region/service/environment/*
+tf_state_dirs=(
+  "./infra/terraform/aws/base/networking/production"
+  "./infra/terraform/aws/base/eks/production"
+  "./infra/terraform/aws/ap-southeast-3/antarticite/production"
+)
+
+{
+for dir in "${tf_state_dirs[@]}"; do
+    (cd "$dir" && terraform show)
+done 
+} | less
+
